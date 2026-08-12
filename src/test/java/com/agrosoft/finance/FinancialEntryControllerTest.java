d3968c4616702a5c7be2503ed29e824843b7e47e
Mihajlo1612 <mradojevic016@gmail.com>
2026-08-12 17:32:18 +0200

    Add integration test for financial entry creation 403 case

--- src/test/java/com/agrosoft/finance/FinancialEntryControllerTest.java
+++ src/test/java/com/agrosoft/finance/FinancialEntryControllerTest.java
@@ -25,19 +25,21 @@ import com.agrosoft.farm.FarmResponse;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestRestTemplate
 public class FinancialEntryControllerTest {
-    
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     private String registerAndLogin() {
         String uuid = UUID.randomUUID().toString();
-        RegisterRequest registerRequest = new RegisterRequest("user" + uuid + "@test.com", "user" + uuid, "lozinka123", "lozinka123");
+        RegisterRequest registerRequest = new RegisterRequest("user" + uuid + "@test.com", "user" + uuid, "lozinka123",
+                "lozinka123");
 
         restTemplate.postForEntity("/api/auth/register", registerRequest, String.class);
 
         LoginRequest loginRequest = new LoginRequest("user" + uuid, "lozinka123");
 
-        ResponseEntity<JwtResponse> loginResponse = restTemplate.postForEntity("/api/auth/login", loginRequest, JwtResponse.class);
+        ResponseEntity<JwtResponse> loginResponse = restTemplate.postForEntity("/api/auth/login", loginRequest,
+                JwtResponse.class);
 
         return loginResponse.getBody().getToken();
     }
@@ -55,10 +57,12 @@ public class FinancialEntryControllerTest {
 
         Long farmId = response.getBody().getId();
 
-        FinancialEntryRequest financialRequest = new FinancialEntryRequest(EntryType.RASHOD, BigDecimal.valueOf(454.234), EntryCategory.POTROSNA_ROBA, 2026, 4, true, "Jagode");
+        FinancialEntryRequest financialRequest = new FinancialEntryRequest(EntryType.RASHOD,
+                BigDecimal.valueOf(454.234), EntryCategory.POTROSNA_ROBA, 2026, 4, true, "Jagode");
 
         HttpEntity<FinancialEntryRequest> financialEntity = new HttpEntity<>(financialRequest, headers);
-        ResponseEntity<FinancialEntryResponse> financialResponse = restTemplate.postForEntity("/api/farms/" + farmId + "/entries", financialEntity, FinancialEntryResponse.class);
+        ResponseEntity<FinancialEntryResponse> financialResponse = restTemplate
+                .postForEntity("/api/farms/" + farmId + "/entries", financialEntity, FinancialEntryResponse.class);
 
         assertThat(financialResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
@@ -70,46 +74,78 @@ public class FinancialEntryControllerTest {
         headers.setBearerAuth(token);
 
         FarmRequest farmRequest = new FarmRequest("Radojevic", "Vranovo", BigDecimal.valueOf(30.1));
-        
+
         HttpEntity<FarmRequest> entity = new HttpEntity<>(farmRequest, headers);
         ResponseEntity<FarmResponse> response = restTemplate.postForEntity("/api/farms", entity, FarmResponse.class);
 
         Long farmId = response.getBody().getId();
 
         HttpEntity<Void> entity2 = new HttpEntity<>(headers);
-        ResponseEntity<String> financialResponse = restTemplate.exchange("/api/farms/" + farmId + "/entries/" + Long.MAX_VALUE, HttpMethod.GET, entity2, String.class);
+        ResponseEntity<String> financialResponse = restTemplate
+                .exchange("/api/farms/" + farmId + "/entries/" + Long.MAX_VALUE, HttpMethod.GET, entity2, String.class);
 
         assertThat(financialResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
     @Test
     public void financeDetails_returns403_whenNotOwner() {
-         String ownerToken = registerAndLogin();
-         String otherToken = registerAndLogin();
+        String ownerToken = registerAndLogin();
+        String otherToken = registerAndLogin();
 
-         HttpHeaders headers = new HttpHeaders();
-         headers.setBearerAuth(ownerToken);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(ownerToken);
 
-         HttpHeaders headers2 = new HttpHeaders();
-         headers2.setBearerAuth(otherToken);
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.setBearerAuth(otherToken);
 
-         FarmRequest farmRequest = new FarmRequest("Draganova farma", "Seone", BigDecimal.valueOf(13.2));
+        FarmRequest farmRequest = new FarmRequest("Draganova farma", "Seone", BigDecimal.valueOf(13.2));
 
-         HttpEntity<FarmRequest> entity = new HttpEntity<>(farmRequest, headers);
-         ResponseEntity<FarmResponse> response = restTemplate.postForEntity("/api/farms", entity, FarmResponse.class);
+        HttpEntity<FarmRequest> entity = new HttpEntity<>(farmRequest, headers);
+        ResponseEntity<FarmResponse> response = restTemplate.postForEntity("/api/farms", entity, FarmResponse.class);
 
-         Long farmId = response.getBody().getId();
+        Long farmId = response.getBody().getId();
 
-         FinancialEntryRequest financialRequest = new FinancialEntryRequest(EntryType.PRIHOD, BigDecimal.valueOf(400.000), EntryCategory.MASINA, 2026, 7, true, "Setvospremac");
+        FinancialEntryRequest financialRequest = new FinancialEntryRequest(EntryType.PRIHOD,
+                BigDecimal.valueOf(400.000), EntryCategory.MASINA, 2026, 7, true, "Setvospremac");
 
-         HttpEntity<FinancialEntryRequest> entity2 = new HttpEntity<>(financialRequest, headers);
-        ResponseEntity<FinancialEntryResponse> financialResponse = restTemplate.postForEntity("/api/farms/" + farmId + "/entries", entity2, FinancialEntryResponse.class);
+        HttpEntity<FinancialEntryRequest> entity2 = new HttpEntity<>(financialRequest, headers);
+        ResponseEntity<FinancialEntryResponse> financialResponse = restTemplate
+                .postForEntity("/api/farms/" + farmId + "/entries", entity2, FinancialEntryResponse.class);
 
         Long entryId = financialResponse.getBody().getId();
 
         HttpEntity<Void> entity3 = new HttpEntity<>(headers2);
-        ResponseEntity<String> finalResponse = restTemplate.exchange("/api/farms/" + farmId + "/entries/" + entryId, HttpMethod.GET, entity3, String.class);
+        ResponseEntity<String> finalResponse = restTemplate.exchange("/api/farms/" + farmId + "/entries/" + entryId,
+                HttpMethod.GET, entity3, String.class);
 
         assertThat(finalResponse.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
     }
- }
+
+    @Test
+    public void createEntry_returns403_whenNotOwner() {
+        String ownerToken = registerAndLogin();
+        String otherToken = registerAndLogin();
+
+        HttpHeaders ownerHeaders = new HttpHeaders();
+        ownerHeaders.setBearerAuth(ownerToken);
+
+        HttpHeaders otherHeaders = new HttpHeaders();
+        otherHeaders.setBearerAuth(otherToken);
+
+        FarmRequest farmRequest = new FarmRequest("Radojeva farma", "Binovac", BigDecimal.valueOf(12.5));
+
+        HttpEntity<FarmRequest> entity = new HttpEntity<>(farmRequest, ownerHeaders);
+        ResponseEntity<FarmResponse> response = restTemplate.postForEntity("/api/farms", entity, FarmResponse.class);
+
+        Long farmId = response.getBody().getId();
+
+        FinancialEntryRequest financialRequest = new FinancialEntryRequest(EntryType.PRIHOD,
+                BigDecimal.valueOf(400.000), EntryCategory.MASINA, 2026, 7, true, "Setvospremac");
+
+        HttpEntity<FinancialEntryRequest> entity2 = new HttpEntity<>(financialRequest, otherHeaders);
+        ResponseEntity<String> financialResponse = restTemplate.postForEntity("/api/farms/" + farmId + "/entries",
+                entity2, String.class);
+
+        assertThat(financialResponse.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
+}
